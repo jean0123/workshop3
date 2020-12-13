ca487489d3fea0a9597a569525d68d1fbc502e67
unknown <jean93pierre06@gmail.com>
2020-12-13 01:20:19 -0500

    Finalización del reto semana 3

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 # semana-3
-=======
-# workshop3
->>>>>>> 46393e49db2c579bc66ef52ec8e2d3371a980289
--- server.js
+++ server.js
@@ -1,32 +1,43 @@
-/*en caso de  hacer uso con el directorio controlador se 
-debe importar como se observa en la siguiente linea, con el nombre del archivo js
-que contiene la logica */
-//const controller = require('./controller/nombredelcontrollador.js');
+/* const controller = require('./controllers/UserController.js'); */
 const express = require('express');
-const db = require('./models');
-const app = express()
+const morgan = require('morgan');
+const apiRouter = require('./routes/index');
+/* const db = require('./models'); */
 const bodyParser = require('body-parser');
-app.use(function(req, res, next) {
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content - Type, Accept");
+    res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
     next();
 });
 
-
+app.use(morgan('dev'));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// API ENDPOINTS
-/*se debe contar un una ruta por medio de método post para el inicio de sesión de la siguiente manera:
-'/api/auth/signin'
-*/
-app.get('/', function(req, res) {
-    console.log("Estructura base del proyecto backend");
-    res.send("Estructura base del proyecto backend");
+/* // API ENDPOINTS
+app.get('/api/users', (req, res) => {
+    db.user.findAll().then(users => res.json(users))
 });
-const port = 3000
-app.listen(port, () => {
-    console.log(`Running on http://localhost:${port}`)
-})
+app.post('/api/auth/signin', controller.signin);
+ */
 
+app.use('/api', apiRouter);
+
+app.set('PORT', process.env.PORT || 3000);
+
+app.get('/', function (req, res) {
+    console.log("Extructura base del proyecto backend");
+    res.send("Estructura base del proyecto backend");
+    /* db.user.findAll().then(users => res.json(users)) */
+});
+/* const port = 3000 */
+app.listen(app.get('PORT'), () => {
+    console.log(`Running on http://localhost:${app.get('PORT')}`)
+})
 module.exports = app;
